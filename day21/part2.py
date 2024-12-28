83726d98c14551d57cfa13ac311bb82d61c6bd11
Kau Khai Kit <74185102+exellaz@users.noreply.github.com>
2024-12-28 21:00:05 +0800

    day21 complete

---
+++ day21/part2.py
@@ -0,0 +1,35 @@
+# Answer: 129551515895690
+
+from collections import Counter
+
+def build_grid(keys):
+    return {c: (i % 3, i // 3) for i, c in enumerate(keys)}
+
+def steps(grid, seq, weight):
+    px, py = grid["A"]
+    bx, by = grid[" "]
+    result = Counter()
+    for instruction in seq:
+        npx, npy = grid[instruction]
+        block = (npx == bx and py == by) or (npy == by and px == bx)
+        result[(npx - px, npy - py, block)] += weight
+        px, py = npx, npy
+    return result
+
+def get_complexity(codes, n, keypad, dirpad):
+    total = 0
+    for code in codes:
+        res = steps(keypad, code, 1)
+        for _ in range(n + 1):
+            new_res = Counter()
+            for (x, y, block), count in res.items():
+                seq = ("<" * -x + "v" * y + "^" * -y + ">" * x)[::-1 if block else 1] + "A"
+                new_res += steps(dirpad, seq, count)
+            res = new_res
+        total += res.total() * int(code[:3])
+    return total
+
+codes = open("input.txt").read().split("\n")
+keypad = build_grid("789456123 0A")
+dirpad = build_grid(" ^A<v>")
+print(get_complexity(codes, 25, keypad, dirpad))
